d4a37d39c53034c5f0d9976155e7942638c51f47
Sebastian Proksch <development@mail.proks.ch>
2022-04-23 03:56:38 +0200

    Add missing flag

--- releng/set_VersionImplTest_constant.sh
+++ releng/set_VersionImplTest_constant.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 V=$1
 TEST=`find . -name "VersionImplTest.java"`
-sed "s/CURRENT_VERSION = \"[^\"]*\"/CURRENT_VERSION = \"${V}\"/" $TEST
+sed -i "s/CURRENT_VERSION = \"[^\"]*\"/CURRENT_VERSION = \"${V}\"/" $TEST
